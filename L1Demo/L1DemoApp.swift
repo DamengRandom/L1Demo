ca8ea077bb3be14f8636b205442309ac5cf493c6
damengrandom <damonmaozewu@gmail.com>
2023-03-22 20:57:12 +1100

    Initial Commit

---
+++ L1Demo/L1DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  L1DemoApp.swift
+//  L1Demo
+//
+//  Created by Damon Wu on 22/3/2023.
+//
+
+import SwiftUI
+
+@main
+struct L1DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
