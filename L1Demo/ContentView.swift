08e6e541d8515e5d3afbea4a235fd896f6dbb533
damengrandom <damonmaozewu@gmail.com>
2023-03-24 21:16:40 +1100

    initial trial for build iOS app

--- L1Demo/ContentView.swift
+++ L1Demo/ContentView.swift
@@ -9,13 +9,57 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            Color(.white).ignoresSafeArea()
+            VStack (alignment: .leading, spacing: 20.0) {
+                Image("lofi_2023_damon")
+                    .resizable()
+                    .cornerRadius(15)
+                    .aspectRatio(contentMode: .fit)
+//                    .padding(.all)
+                HStack {
+                    Text("Lofi 2023 Damon Image").font(.title2).fontWeight(.bold).foregroundColor(Color.black)
+                    
+                    Spacer()
+                    
+                    VStack{
+                        HStack {
+                            Image(systemName: "star.fill").foregroundColor(.orange)
+                                .font(.caption)
+                            Image(systemName: "star.fill")
+                                .foregroundColor(.orange)
+                                    .font(.caption)
+                            Image(systemName: "star.fill")
+                                .foregroundColor(.orange)
+                                    .font(.caption)
+                            Image(systemName: "star.fill")
+                                .foregroundColor(.orange)
+                                    .font(.caption)
+                            Image(systemName: "star.leadinghalf.filled")
+                                .foregroundColor(.orange)
+                                    .font(.caption)
+                            
+                        }
+                        Text("(Reviews 361)")
+                    }
+                    // SF symbols: Apple free icons
+                }
+                
+                Text("The description text for demo only ...")
+                
+                HStack {
+                    Spacer()
+                    
+                    Image(systemName: "fork.knife")
+                    Image(systemName: "binoculars.fill")
+                }
+                .foregroundColor(.yellow)
+                .font(.caption)
+            }.padding().background(Rectangle().foregroundColor(.teal))
+                .cornerRadius(15)
+                .shadow(radius: 15)
+                .padding()
         }
-        .padding()
     }
 }
 
